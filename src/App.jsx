e932a2a01836a8a19d6f751e2a30e53ebcbc5d12
palmahadeb <119657686+palmahadeb@users.noreply.github.com>
2023-05-27 14:36:05 +0600

    login and Registration design complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import {
   createRoutesFromElements
 } from "react-router-dom";
 import Registration from "./Pages/Registration";
+import Login from "./Pages/Login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
+   <Route>
     <Route path="/" element={<Registration />} ></Route>
+    <Route path="/login" element={<Login />} ></Route>
+   </Route>
   )
 );
 
